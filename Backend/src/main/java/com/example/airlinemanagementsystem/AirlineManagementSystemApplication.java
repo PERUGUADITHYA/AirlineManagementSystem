a92e5ace057d8e4dcb8cbd9aacf04575b021d08f
PERUGU ADITHYA <adithyaperugu2021@gmail.com>
2024-09-13 18:02:16 +0530

    initial commit

---
+++ Backend/src/main/java/com/example/airlinemanagementsystem/AirlineManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.airlinemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineManagementSystemApplication.class, args);
+	}
+
+}
